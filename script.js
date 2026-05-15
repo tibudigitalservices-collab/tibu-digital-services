1e0c2eb7e259bc478e0c5ecc53d9d20ccd8540e3
tibudigitalservices-collab <tibudigitalservices@gmail.com>
2026-05-15 17:46:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const navLinks = document.querySelector(".nav-links");
 if (menuButton && navLinks) {
   menuButton.addEventListener("click", () => {
     navLinks.classList.toggle("open");
-
     const isOpen = navLinks.classList.contains("open");
     menuButton.setAttribute("aria-expanded", isOpen ? "true" : "false");
   });
